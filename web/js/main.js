206e671cd0328e7c0165fc8b729c84e2a42aa43b
Kaede Fujisaki <kaede@hexe.net>
2016-07-18 17:14:19 +0900

    divided to img+page

--- web/js/main.js
+++ web/js/main.js
@@ -9,22 +9,53 @@
    * @param {string} url
    * @param {number} width
    * @param {number} height
+   * @constructor
    */
-  Minobi.Page = function(url, width, height) {
-    /** @type {string} */
+  Minobi.Image = function(url, width, height) {
     this.url = url;
-    /** @type {number} */
     this.width = width;
-    /** @type {number} */
+    this.height = height;
+
+    /** @type {!HTMLImageElement} element */
+    this.element = document.createElement('img');
+  };
+
+  /**
+   * @param {Minobi.Image[]} images
+   * @param {number} width
+   * @param {number} height
+   * @constructor
+   */
+  Minobi.Page = function(images, width, height) {
+    this.images = images;
+    this.width = width;
     this.height = height;
     /** @type {Minobi.Page} */
     this.prev = null;
     /** @type {Minobi.Page} */
     this.next = null;
-    /** @type {HTMLImageElement} */
-    this.elem = null;
+    /** @type {HTMLDivElement} */
+    this.elem = document.createElement('div');
+
+    this.elem.className = 'manga-page';
+    /** @type {!HTMLDivElement[]} */
+    this.imageContainers = [];
+    var left = 0;
+    for(var i = 0; i < this.images.length; i++) {
+      /** @type {HTMLDivElement} */
+      var img = images[i];
+      var elem = img.element;
+      elem.classList.add('manga-page-wrap');
+      this.imageContainers.push(elem);
+      this.elem.appendChild(elem);
+      elem.style.left = left + 'px';
+      left += img.width;
+    }
   };
   Minobi.Page.prototype = {
+    attached: function() {
+      return !this.elem || !!this.elem.parentElement;
+    },
     /**
      * @param {HTMLDivElement} container
      */
@@ -33,6 +64,7 @@
         console.error("Null elem.");
         return;
       } else if(container === this.elem.parentElement) {
+        console.warn("already attached");
         return;
       }
       container.appendChild(this.elem);
@@ -44,7 +76,8 @@
       if(!this.elem) {
         console.error("Null elem.");
         return;
-      } else if(container === this.elem.parentElement) {
+      } else if(container !== this.elem.parentElement) {
+        console.warn("already detached");
         return;
       }
       container.removeChild(this.elem);
@@ -60,6 +93,7 @@
   };
 
   /*** Cache System ***/
+
   /**
    * @param {Minobi.Viewer} viewer
    * @param {Minobi.Chapter} chapter
@@ -68,36 +102,37 @@
   Minobi.ImageCache = function(viewer, chapter) {
     this.viewer_ = viewer;
     this.chapter_ = chapter;
-    /** @type {Minobi.ImageCache.Entry[]} */
-    this.entries_ = [];
-    for(var i = 0;i < chapter.pages.length; i++) {
-      var page = chapter.pages[i];
-      var ent = new Minobi.ImageCache.Entry(page);
-      this.entries_.push(ent);
-    }
   };
-  /**
-   * @param {Minobi.Page} page
-   * @constructor
-   */
-  Minobi.ImageCache.Entry = function(page) {
-    /** @type {Minobi.Page} */
-    this.page_ = page;
-    /** @type {HTMLImageElement} */
-    this.element_ = null;
-  };
-  Minobi.ImageCache.Entry.prototype = {
-    load: function() {
+
+  Minobi.ImageCache.prototype = {
+    /**
+     * @param {Minobi.Page} page
+     */
+    enqueue: function(page) {
+      // TODO: implement real cache.
+      for(var i=0; i < page.images.length; i++) {
+        this.setImage_(page.images[i], page.images[i].url);
+      }
+    },
+    /**
+     * @param {Minobi.Image} img
+     * @param {string} url
+     */
+    setImage_: function(img, url) {
       if(!this.element_) {
         /** @type {HTMLImageElement} */
         this.element_ = document.createElement("img");
         this.element_.className = 'manga-page';
-        this.element_.src = this.page_.url;
+        this.element_.src = url;
       }
     },
-    clear: function() {
-      if(this.element_) {
-        this.element_.src = "";
+    /**
+     * @param {Minobi.Image} img
+     */
+    clear: function(img) {
+      if(img.element){
+        img.element.src = "";
+        img.element = null;
       }
     }
   };
@@ -112,21 +147,28 @@
   Minobi.Viewer = function(container, chapter) {
     /** @type {HTMLDivElement} */
     this.container_ = container;
-    /** @type {Minobi.Chapter} */
     this.chapter = chapter;
     this.cache_ = new Minobi.ImageCache(this, chapter);
-    var self = this;
+
+    // Axis
+    this.x = new Minobi.Axis(chapter, chapter.pages[0], 'width');
+    this.y = new Minobi.Axis(chapter, chapter.pages[0], 'height');
+
+    //
+    this.container_.classList.add('minobi');
     this.container_.addEventListener('load', function() {
-      self.render();
-    });
-    /** @type {Minobi.Cursor} */
-    this.cursor_ = new Minobi.Cursor(this.chapter.pages[0]);
+      this.render();
+    }.bind(this));
   };
+
   Minobi.Viewer.prototype = {
     /**
      * @param {number|undefined} initPage
      */
     init: function(initPage) {
+      initPage ||= 0;
+      this.x.set(this.chapter.pages[initPage]);
+      this.y.set(this.chapter.pages[initPage]);
     },
     render: function() {
     }
@@ -143,9 +185,24 @@
     this.current = page;
     this.axis = axis;
     this.lastMoved = -1;
-    this.pos = 0;
+    this.pos = 0
+    this.speed = 0;
   };
   Minobi.Axis.prototype = {
+    reset: function(){
+      this.pos = 0;
+      this.speed = 0;
+    },
+    /**
+     * @param {!Minobi.Page} page
+     */
+    set: function(page){
+      this.current = page;
+      this.reset();
+    },
+    get enabled() {
+      return this.pos > 1 || this.pos < -1;
+    }
   };
 
   /**
@@ -163,102 +220,6 @@
   };
 
   /**
-   * @param {Minobi.Viewer} viewer
-   * @param {Minobi.Page} page
-   * @constructor
-   */
-  Minobi.Cursor = function(viewer, page) {
-    this.viewer = viewer;
-    this.x = 0;
-    this.y = 0;
-    this.vx = 0;
-    this.xy = 0;
-    /** @type {Minobi.Page} */
-    this.page = page;
-    this.lastMoved = -1;
-  };
-  Minobi.Cursor.prototype = {
-    /**
-     * @param {Minobi.Cursor} cursor
-     */
-    feedback: function() {
-      var hwidth = this.page.width/2;
-      var hheight = this.page.height/2;
-      var absX = Math.abs(this.x);
-      var absY = Math.abs(this.y);
-      if(this.lastMoved < 0) {
-        this.lastMoved = new Date().getTime();
-      }
-      var now = new Date().getTime();
-      var deltaT = (now - this.lastMoved) / 1000.0;
-      this.lastMoved = now;
-      if(absX > Minobi.Cursor.Epsilon){
-        // Sliding on X-axis
-        this.vx -= Minobi.Cursor.Spring * this.x;
-        var diff = this.vx * deltaT;
-        this.x += diff;
-        if(this.x * (this.x - diff) < 0) {
-          this.vx = 0;
-          this.x = 0;
-        } else if(this.x < -hwidth) {
-          if(this.page.next) {
-            this.page.next.detatch(this.viewer.container_);
-          }
-          this.page = this.page.prev;
-          this.x += hwidth + (this.page.width/2);
-        } else if (this.x > hwidth) {
-          if(this.page.prev) {
-            this.page.prev.detatch(this.viewer.container_);
-          }
-          this.page = this.page.next;
-          this.x -= hwidth + (this.page.width/2);
-        }
-
-        // do not slide on y-axis.
-        this.vy = 0;
-        this.y = 0;
-      } else if(absY > Minobi.Cursor.Epsilon) {
-        // Sliding on X-axis
-        this.vy -= Minobi.Cursor.Spring * this.y;
-        var diff = this.vy * deltaT;
-        this.y += diff;
-        if(this.y * (this.y - diff) < 0) {
-          this.vy = 0;
-          this.y = 0;
-        } else if(this.y < -hheight) {
-          this.page = this.page.prev;
-          this.y += hheight + (this.page.height/2);
-        } else if (this.y > hheight) {
-          this.page = this.page.next;
-          this.y -= hheight + (this.page.height/2);
-        }
-
-        // do not slide on y-axis.
-        this.vx = 0;
-        this.x = 0;
-      } else {
-      }
-    },
-    /**
-     * @param {number} dx
-     * @param {number} dy
-     */
-    move: function(dx, dy) {
-      var absX = Math.abs(this.x);
-      var absY = Math.abs(this.y);
-      if(absX > Minobi.Cursor.Epsilon){
-        this.x += dx;
-        this.vx = 0;
-      } else if(absY > Minobi.Cursor.Epsilon) {
-        this.y += dy;
-        this.vy = 0;
-      }
-    }
-  }
-  Minobi.Cursor.Epsilon = 1;
-  Minobi.Cursor.Spring = 0.1;
-
-  /**
    * @param {HTMLDivElement} container
    * @param {[{path: string, width: number, height:number}]} chapter
    */
@@ -266,7 +227,8 @@
     /** @type {[Minobi.Page]} */
     var pages = [];
     for(var i=0;i < chapter.length; i++) {
-      var page = new Minobi.Page(chapter[i].path, chapter[i].width, chapter[i].height);
+      var image = new Minobi.Image(chapter[i].path, chapter[i].width, chapter[i].height);
+      var page = new Minobi.Page([image], chapter[i].width, chapter[i].height);
       pages.push(page);
       if(i > 0) {
         pages[i-1].next = page;
