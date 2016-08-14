e1bc3ba53f419b3671fbf770b3fce0c16e757ac1
Kaede Fujisaki <kaede@hexe.net>
2016-08-15 01:19:19 +0900

    横向きの実装が出来た

--- web/js/main.js
+++ web/js/main.js
@@ -27,6 +27,9 @@
    * @constructor
    */
   Minobi.Page = function(images, width, height) {
+    this.scale_ = 1;
+    this.x_ = 0;
+    this.y_ = 0;
     this.images = images;
     this.width = width;
     this.height = height;
@@ -36,8 +39,11 @@
     this.next = null;
     /** @type {HTMLDivElement} */
     this.elem = document.createElement('div');
-
     this.elem.className = 'manga-page';
+    /** @type {HTMLDivElement} */
+    var inner = document.createElement('div');
+    inner.className = 'manga-page';
+    this.elem.appendChild(inner);
     /** @type {!HTMLDivElement[]} */
     this.imageContainers = [];
     var left = 0;
@@ -47,7 +53,7 @@
       var elem = img.element;
       elem.classList.add('manga-page-wrap');
       this.imageContainers.push(elem);
-      this.elem.appendChild(elem);
+      inner.appendChild(elem);
       elem.style.left = left + 'px';
       left += img.width;
     }
@@ -81,6 +87,45 @@
         return;
       }
       container.removeChild(this.elem);
+    },
+    /**
+     * @param {number} v
+     */
+    set scale(scale) {
+      this.scale_ = scale;
+      this.elem.style.transform = 'scale('+scale+','+scale+')';
+    },
+    /**
+     * @return {number} scale
+     */
+    get scale() {
+      return this.scale_;
+    },
+    /**
+     * @param {number} x
+     */
+    set x(x) {
+      this.x_ = x;
+      this.elem.style.left = x + 'px';
+    },
+    /**
+     * @return {number} x
+     */
+    get x() {
+      return this.x_;
+    },
+    /**
+     * @param {number} x
+     */
+    set y(y) {
+      this.y_ = y;
+      this.elem.style.top = y + 'px';
+    },
+    /**
+     * @return {number} y
+     */
+    get y() {
+      return this.y_;
     }
   };
 
@@ -148,7 +193,7 @@
     this.cache_ = new Minobi.ImageCache(this, chapter);
 
     // Axis
-    this.axis = this.makeXaxis(initPage);
+    this.axis = this.makeHorizontalAxis(initPage);
     this.potential = new Minobi.StaticPotential(this.axis);
     this.cache_.enqueue(chapter.pages[initPage]);
 
@@ -165,54 +210,77 @@
     init: function() {
       this.render();
       this.container_.addEventListener('keyup', function(event) {
+        var reload = false;
         switch(event.keyCode) {
         case 38: //up - previous
+          reload = this.axis.onUp();
           break;
         case 40: //down - next
+          reload = this.axis.onDown();
           break;
         case 37:// left - next
+          reload = this.axis.onLeft();
           break;
         case 39: //right - previous
+          reload = this.axis.onRight();
           break;
-        }sus
-      });
+        }
+        if(reload) {
+          this.render();
+        }
+      }.bind(this));
     },
     render: function() {
-      var strategy = this.axis.strategy;
-      var page = this.axis.current;
-      var scale = strategy(this.container_.clientWidth/page.width, this.container_.clientHeight / page.height);
-      page.elem.style.transform = 'scale('+scale+','+scale+')';
-      page.attach(this.container_);
+      this.axis.render(this);
+    },
+    /**
+     * @param {Minobi.Page} page
+     * @return {boolean} r
+     */
+    renderPage: function(page, deltaX, deltaY) {
+      var scale = this.axis.calcScale(this.container_, page);
+      var w = page.width * scale;
+      var h = page.height * scale;
+      var offX = (this.container_.clientWidth - w) / 2;
+      var offY = (this.container_.clientHeight - h) / 2;
+      page.scale = scale;
+      page.x = offX + deltaX;
+      page.y = offY + deltaY;
+      var out =
+        page.x + w <= 0 || page.x >= this.container_.clientWidth ||
+        page.y + h <= 0 || page.y >= this.container_.clientHeight;
+      if(!out) {
+        page.attach(this.container_);
+      }
+      this.cache_.enqueue(page);
+      return !out;
     },
     /**
      * @param {number} pageNum
      */
-    makeXaxis: function(pageNum) {
-      return new Minobi.Axis(this.chapter, this.chapter.pages[pageNum], 'width', Math.min);
+    makeHorizontalAxis: function(pageNum) {
+      return new Minobi.HorizontalAxis(this.chapter, this.chapter.pages[pageNum]);
     },
     /**
      * @param {number} pageNum
      */
-    makeYaxis: function(pageNum) {
-      return new Minobi.Axis(this.chapter, this.chapter.pages[pageNum], 'height', Math.max);
+    makeVertivalAxis: function(pageNum) {
+      return new Minobi.VerticalAxis(this.chapter, this.chapter.pages[pageNum]);
     }
   };
 
   /**
    * @param {Minobi.Chapter} chapter
    * @param {Minobi.Page} page
-   * @param {!string} axis
-   * @param {!function(number, number)} strategy
    * @constructor
    */
-  Minobi.Axis = function(chapter, page, axis, strategy) {
+  Minobi.Axis = function(chapter, page) {
     this.chapter = chapter;
+    /** @type {Minobi.Page} */
     this.current = page;
     this.lastMoved = -1;
     this.pos = 0
     this.speed = 0;
-    this.axis = axis;
-    this.strategy = strategy;
   };
 
   Minobi.Axis.prototype = {
@@ -221,16 +289,176 @@
       this.speed = 0;
     },
     /**
+     * @param {HTMLDivElement} container
+     * @param {Minobi.Page} page
+     */
+    calcScale: function(container, page) {
+      throw new Error("Please implement Minobi.Axis.calcScale");
+    },
+    /**
      * @param {!Minobi.Page} page
      */
     set: function(page){
       this.current = page;
       this.reset();
     },
-    get enabled() {
-      return this.pos > 1 || this.pos < -1;
+    /**
+     * @param {Minobi.Viewer} viewer
+     */
+    render: function(viewer) {
+      throw new Error("Please implement Minobi.Axis.render");
+    },
+    /**
+     * @param {Minobi.Viewer} viewer
+     * @return {boolean} reload
+     */
+    onUp: function(viewer) {
+      return false;
+    },
+    /**
+     * @return {boolean} reload
+     */
+    onDown: function(viewer) {
+    },
+    /**
+     * @return {boolean} reload
+     */
+    onLeft: function(viewer) {
+    },
+    /**
+     * @return {boolean} reload
+     */
+    onRight: function(viewer) {
     }
   };
+
+  /**
+   * @param {Minobi.Chapter} chapter
+   * @param {Minobi.Page} page
+   * @extends {Minobi.Axis}
+   * @constructor
+   */
+  Minobi.HorizontalAxis = function(chapter, page) {
+    Minobi.Axis.call(this, chapter, page);
+  };
+
+  Minobi.HorizontalAxis.prototype = Object.create(Minobi.Axis.prototype, {
+    calcScale: {
+      /**
+       * @param {HTMLDivElement} container
+       * @param {Minobi.Page} page
+       */
+      value: function(container, page) {
+        return Math.min(container.clientWidth / page.width, container.clientHeight / page.height);
+      }
+    },
+    render: {
+      /**
+       * @param {Minobi.Viewer} viewer
+       */
+      value: function(viewer) {
+        var dx = this.pos;
+        viewer.renderPage(this.current, dx, 0);
+        var offX = dx - (this.current.width * this.current.scale);
+        for(var page = this.current.next; !!page; page = page.next) {
+          if(!viewer.renderPage(page, offX, 0)) {
+            break;
+          }
+          offX += -(page.width * page.scale);
+        }
+        offX = dx + (this.current.width * this.current.scale);
+        for(var page = this.current.prev; !!page; page = page.prev) {
+          if(!viewer.renderPage(page, offX, 0)) {
+            break;
+          }
+          offX += (page.width * page.scale);
+        }
+      }
+    },
+    onLeft: {
+      /**
+       * @return {boolean} reload
+       */
+      value: function() {
+        if(this.current.next) {
+          this.set(this.current.next);
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    onRight: {
+      /**
+       * @return {boolean} reload
+       */
+      value: function() {
+        if(this.current.prev) {
+          this.set(this.current.prev);
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  });
+
+  /**
+   * @param {Minobi.Chapter} chapter
+   * @param {Minobi.Page} page
+   * @extends {Minobi.Axis}
+   * @constructor
+   */
+  Minobi.VerticalAxis = function(chapter, page) {
+    Minobi.Axis.call(this, chapter, page);
+  };
+  Minobi.VerticalAxis.prototype = Object.create(Minobi.Axis.prototype, {
+    calcScale: {
+      /**
+       * @param {HTMLDivElement} container
+       * @param {Minobi.Page} page
+       */
+        value: function(container, page) {
+        return container.clientWidth / page.width;
+      }
+    },
+    render: {
+      /**
+       * @param {Minobi.Viewer} viewer
+       */
+      value: function(viewer) {
+        //TODO: implement vertical.
+        //基本的にはページの頭がブラウザ上でも頭に来るようにレンダリングすればOK
+        viewer.renderPage(this.current, 0, 0);
+      }
+    },
+    onDown: {
+      /**
+       * @return {boolean} reload
+       */
+      value: function() {
+        if(this.current.next) {
+          this.set(this.current.next);
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    onUp: {
+      /**
+       * @return {boolean} reload
+       */
+      value: function() {
+        if(this.current.prev) {
+          this.set(this.current.prev);
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  });
 
   /**
    * @param {Minobi.Axis} axis
@@ -251,11 +479,13 @@
    * @constructor
    */
   Minobi.StaticPotential = function(axis) {
-    Minobi.Potential.apply(this);
+    Minobi.Potential.call(this);
   };
   Minobi.StaticPotential.prototype = Object.create(Minobi.Potential.prototype, {
-    feedback: function() {
-      // Do not move at all.
+    feedback: {
+      value: function() {
+        // Do not move at all.
+      }
     }
   });
 
